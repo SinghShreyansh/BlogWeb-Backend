be703c82089f69538765d8c5fc74129ce282ddc6
Shreyansh Singh <91277635+SinghShreyansh@users.noreply.github.com>
2022-03-31 15:09:24 +0530

    Update settings.py

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -126,7 +126,7 @@ APPEND_SLASH=False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
