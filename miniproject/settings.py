f4afa4727ff60e824d00684fb33588dc26d40060
Shreyansh Singh <91277635+SinghShreyansh@users.noreply.github.com>
2022-03-31 15:24:20 +0530

    Update settings.py

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 # CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
