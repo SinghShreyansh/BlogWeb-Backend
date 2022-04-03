d7b555f66308e425cdc7a857ee36bd0790932ad8
Shreyansh Singh <2020.shreyansh.singh@ves.ac.in>
2022-04-03 23:23:27 +0530

    11th commit

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$e&!n@51(txfc&(eqojg#36^v-dl$0o)^qfzneckc=%ri8e$s8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'blog-web-django.herokuapp.com']
 
 
 # Application definition
