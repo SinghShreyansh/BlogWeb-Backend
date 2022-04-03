1a8f4c69ca5d272a1495c3421db87287680111cf
Shreyansh Singh <2020.shreyansh.singh@ves.ac.in>
2022-04-03 23:02:42 +0530

    9th commit

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -135,7 +135,7 @@ import os
 STATIC_URL = 'static/'
 # DISABLE_COLLECTSTATIC=1
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
