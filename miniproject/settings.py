da38385bf7637b00401bae288499d2509e29aa52
Shreyansh Singh <2020.shreyansh.singh@ves.ac.in>
2022-04-03 22:57:05 +0530

    changes made

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -133,6 +133,7 @@ APPEND_SLASH=False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 import os
 STATIC_URL = 'static/'
+DISABLE_COLLECTSTATIC=1
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
